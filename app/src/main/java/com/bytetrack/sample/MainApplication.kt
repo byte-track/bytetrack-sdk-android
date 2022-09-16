58a842a99f394ba5ebd09073bea56b2f7811d331
刘柱 <liuzhucool@163.com>
2022-09-16 16:54:06 +0800

    隐藏信息

--- app/src/main/java/com/bytetrack/sample/MainApplication.kt
+++ app/src/main/java/com/bytetrack/sample/MainApplication.kt
@@ -13,7 +13,7 @@ class MainApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        ByteTrack.instance.initMessager(this, "166", "android_sdk323cce97c9e54cbabe58ace4409c4f1b")
+        ByteTrack.instance.initMessager(this, "your app id", "your app key")
     }
 
 }
