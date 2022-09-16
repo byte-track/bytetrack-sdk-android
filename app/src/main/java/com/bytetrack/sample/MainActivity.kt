93b15bad396bb653858e30119ef61795c6e4184b
刘柱 <liuzhucool@163.com>
2022-09-16 10:28:45 +0800

    1.0.0版本

--- app/src/main/java/com/bytetrack/sample/MainActivity.kt
+++ app/src/main/java/com/bytetrack/sample/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.bytetrack.sample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import android.widget.Toast.LENGTH_SHORT
-import com.bytestrack.android.sdk.BytesTrack
+import androidx.appcompat.app.AppCompatActivity
+import com.bytetrack.android.sdk.ByteTrack
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,15 +20,14 @@ class MainActivity : AppCompatActivity() {
             if (userInfo.isBlank()) {
                 Toast.makeText(this, "请输入用户名", LENGTH_SHORT).show()
             } else {
-                BytesTrack.instance.userLogin("userInfo")
+                ByteTrack.instance.userLogin("111")
                 Toast.makeText(this, "设置成功", LENGTH_SHORT).show()
             }
         }
 
         findViewById<Button>(R.id.bt_logout).setOnClickListener {
-            BytesTrack.instance.userLogout()
+            ByteTrack.instance.userLogout()
             Toast.makeText(this, "退出账号成功", LENGTH_SHORT).show()
-            Toast.makeText(this, "dev退出账号成功", LENGTH_SHORT).show()
         }
     }
 }
